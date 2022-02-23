5f36c52c6488d4a0a7809525e1ef36619f100b0d
Marco Nashaat <marconash2at@gmail.com>
2022-02-23 13:36:50 +0200

    children vs descendants

--- project_2.py
+++ project_2.py
@@ -6,8 +6,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('https://www.pythonscraping.com/pages/warandpeace.html')
+html = urlopen('https://www.pythonscraping.com/pages/page3.html')
 bs = BeautifulSoup(html.read(),'html.parser')
-names = bs.find_all('span',{'class':'green'})
-for name in names:
-    print(name.get_text())
+for child in bs.find('table',{'id':'giftList'}).children:
+    print(child)
+
+for descendant in bs.find('table',{'id':'giftList'}).descendants:
+    print(descendant)
