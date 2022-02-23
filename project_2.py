25ffae1207777b1d36deb973eaf0b58d83cc3903
Marco Nashaat <marconash2at@gmail.com>
2022-02-23 13:37:59 +0200

    siblings & parents

--- project_2.py
+++ project_2.py
@@ -8,8 +8,8 @@ from bs4 import BeautifulSoup
 
 html = urlopen('https://www.pythonscraping.com/pages/page3.html')
 bs = BeautifulSoup(html.read(),'html.parser')
-for child in bs.find('table',{'id':'giftList'}).children:
-    print(child)
 
-for descendant in bs.find('table',{'id':'giftList'}).descendants:
-    print(descendant)
+for sibling in bs.find('table',{'id':'giftList'}).tr.next_siblings:
+    print(sibling)
+
+print(bs.find('img',{'src':'../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
